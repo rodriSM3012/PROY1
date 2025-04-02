27100c18781e25ae12979033117503c6631af65a
rodrigosanchez <30.rodrigo.12@gmail.com>
2025-04-02 12:27:41 +0200

    first commit

---
+++ sabores-del-mundo/tailwind.config.js
@@ -0,0 +1,9 @@
+export const theme = {
+    extend: {
+        fontFamily: {
+            'rosario': ['Rosario', 'sans-serif'], // Usa el mismo nombre que en Google Fonts
+        },
+    },
+};
+export const variants = {};
+export const plugins = [];
